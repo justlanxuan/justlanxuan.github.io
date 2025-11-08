71a9592d6faf5123dcff6dc643a85d415868e0a0
justlanxuan <justlanxuan@gmail.com>
2025-11-08 21:29:57 +0800

    add post

--- data/posts.ts
+++ data/posts.ts
@@ -5,8 +5,22 @@ export const posts = {
     title: "北上游记",
     date: "2024-01-06",
     slug: "shanghai-beijing",
-    excerpt: "和高中的朋友一起去北京看雪",
+    excerpt: "去北京看雪!!!",
     content: "\n## 上海 Shanghai\n第一站到上海主要目的就是找🦙玩～（我是不会说我主要是想去迪士尼的！）然而到达的那天**某人**还在考试，于是我一个人拖着行李到了同济找人蹭饭：感谢我同桌带我去吃超级好吃的花雕鸡海鲜火锅！在上海寒冷的天气加持下，这锅不知道正不正宗的“港式鸡煲”的鲜美超越了我在香港两年吃过的所有火锅。\n<figure class=\"img-row\">\n  <div class=\"img-wrap\">\n  <img src=\"/post/shanghai2.jpg\">\n  <img src=\"/post/shanghai3.jpg\">\n  </div>\n  <figcaption>鲜掉眉毛的花雕鸡火锅</figcaption>\n</figure>\n\n一月份的上海有一种浓郁的秋天的氛围，像我这样常年待在南方的人一开始十分沉浸于这份秋天的浪漫气息：枯黄纷飞的落叶和扑面而来的凉意。但是在街上漫无目的地逛了一个下午之后（中间穿插着好几次冷的受不了进街边的店里避寒），发现我可能还是适合在南方窝一辈子。\n\n一些和🦙的贴贴瞬间～（发pyq被祝99倒是有点离谱了）\n<figure>\n  <img src=\"/post/shanghai1.jpg\">\n  <figcaption>落叶</figcaption>\n</figure>\n<figure>\n  <img src=\"/post/shanghai6.jpg\">\n  <figcaption>落叶++</figcaption>\n</figure>\n<figure class=\"img-row\">\n<div class=\"img-wrap\">\n  <img src=\"/post/shanghai7.jpg\">\n  <img src=\"/post/shanghai9.jpg\">\n</div>\n  <figcaption>隐藏在街边的日本小店，有很多日本的手工艺品（以及落叶+++）</figcaption>\n</figure>\n逛街最舒服的事情就是看到任何好吃的就可以很自然而然地走进去吃（而不必在网上做任何美食攻略），一是饱眼福，二是饱口福。在这么冷的天里我们还是点了全冰沙的星冰乐，并且成功搀到了阿包：）\n<figure class=\"img-row\">\n<div class=\"img-wrap\">\n  <img src=\"/post/shanghai5.jpg\">\n  <img src=\"/post/shanghai8.jpg\">\n</div>\n  <figcaption>甜品和朱古力抹茶星冰乐！</figcaption>\n</figure>\n\n我之前旅行是一个超级无敌J的人，喜欢提前想好所有的攻略和计划，最好是能把所有可能的情况在脑子里过一遍。🦙恰好又是一个，喜欢（并且擅长）在所有的可能性里面开拓出一条新的路的人。但是这一次的旅行反而是打开了我全新的视角：未知和惊喜（虽然也可能是惊吓），去新的地方享受新的生活。如果一切都按照攻略当中的发生了，那么在网上云游似乎就足够了。\n\n于是在上海的这几天里发生了一些按计划发生的事情：去逛复旦的校园，去吃人和馆，去迪士尼玩（超级喜欢疯狂动物城的园区！）；也有计划外的惊喜：所有随机选的店都特别好吃，临时决定去看的豫园灯展，和在迪士尼园区里看完烟花之后再次冲回项目玩直到最后一个出园；还有一些非常离谱的惊吓：“我预感我第二天要生病”-“我高烧了”-“好的我又好了”。这辈子都忘不了半夜三点下楼拿美团送来的药，以及迪士尼酒店里半夜的施工（感觉有些梦碎呢）。\n<figure>\n  <img src=\"/post/shanghai11.jpg\">\n  <figcaption>夜晚的豫园</figcaption>\n</figure>\n\n<figure>\n  <img src=\"/post/shanghai10.jpg\">\n  <figcaption>迪士尼的烟花</figcaption>\n</figure>\n\n## 北京 Beijing\n飞北京的那天恰好是我生日，因此在飞机上获得了空姐送来的小蛋糕（也因此获得了前后排的羡慕）。到北京来找阿包一起过生日！\n<figure>\n  <img src=\"/post/beijing1.jpg\">\n  <figcaption>今年还是比你大一岁！</figcaption>\n</figure>\n羡慕别人家的校园，喜欢清华的大草坪和户外活动的空间，UST你什么时候可以扩建一下！\n<figure>\n  <img src=\"/post/beijing4.jpg\">\n  <figcaption>感谢阿包带我们进去！（嘿嘿，有内部人员就是好）</figcaption>\n</figure>\n作为（自封的）一名巫师，我终于是在19岁的时候来到了霍格沃茨城堡。感觉魔法世界的一切都是如此的真实和欢乐! 快乐就是最强大的守护神咒，这些快乐的瞬间（完全地沉浸地享受这种乐趣）是我生活的最大动力。\n<figure class=\"img-row\">\n<div class=\"img-wrap\">\n  <img src=\"/post/beijing2.jpg\">\n  <img src=\"/post/beijing3.jpg\">\n</div>\n  <figcaption>阿瓦达砍大瓜！</figcaption>\n</figure>\n\n在北京的时候抽空找了一个上午找我的同桌聊了聊，感觉她还是那个能给我一些很特别的视角的人。希望能对未来多一些坚定吧！（聊着聊着有的时候感觉自己有的时候像一个快乐的傻子...）\n\n北京给我的感觉和上海很不一样。我最喜欢北京的地方是他街头巷尾的烟火气。这一点反而是和香港有那么一点相似了，那种蓬勃的、不加掩饰的生活热情。但不同的是，北京的底色在一些古色古香的建筑里，显得更为宽广和醇厚。\n\n故宫是最能体现这种磅礴的气势的。红墙和白雪给人的第一感觉是美，典雅，和庄重。而穿过庄严的前殿来到后宫，空旷而宏大的宫殿变成了层层叠叠的门和院，透过宫墙看到的天空，真的有一入宫门深似海的感觉（突然就对庭院深深的这个“深”字有了同感）。\n<figure>\n  <img src=\"/post/beijing5.jpg\">\n  <figcaption>太和殿</figcaption>\n</figure>\n<figure class=\"img-row\">\n<div class=\"img-wrap\">\n  <img src=\"/post/beijing7.jpg\">\n  <img src=\"/post/beijing6.jpg\">\n</div>\n  <figcaption>庭院深深</figcaption>\n</figure>\n\n就让景山的日落作为这次旅程的终点吧！最遗憾的是看雪之旅没有看到真正的落雪（倒是看到了不少马路上的黑黑的冰渣子，滤镜碎了一地），但是非常幸运地在最后一分钟爬上了景山，看到了北京城的日落。日落即时日出，明天又是新的一天！\n<figure>\n  <img src=\"/post/beijing8.jpg\">\n  <figcaption>景山的日落，从这个角度看有日出的感觉</figcaption>\n</figure>"
+  },
+{
+    title: "马来西亚",
+    date: "2024-08-10",
+    slug: "malaysia",
+    excerpt: "一次旅行和赶due的极限拉扯",
+    content: ""
+  },
+{
+    title: "无锡踏春",
+    date: "2024-03-30",
+    slug: "wuxi",
+    excerpt: "四月天的缤纷落英是物哀的美",
+    content: ""
   },
 {
     title: "ICRA2025",
